d2828ba97943c457ac28293221ec50dfaa9963a6
“Jagadeesh-176” <jagadeeshalamanda176@gmail.com>
2023-08-17 12:04:05 +0000

    “solution”

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import './App.css'
+import GameManager from './Components/GameManager'
+// These are the lists used in the application. You can move them to any component needed.
+
+// Replace your code here
+const App = () => <GameManager />
+
+export default App
